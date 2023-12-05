42134061c97030f909b6820caddb3fe086037b65
Usaid ok <usaidokdiv@gmail.com>
2023-12-05 21:37:59 +0530

    Header Just Completed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,47 +1,35 @@
-import React from 'react'
-import './Header.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './Header.css';
+
 function Header() {
-    return (
-        <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <Link path='' className='shopname'>
-                    Olx
-                </Link>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <div className="search1">
-                                <input type="text" name='Country' placeholder='' className='country-search' />
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <div className="search2">
-                                <button className='searchbtn'>
-                                    <input type="text" name='Country'
-                                        placeholder='Find Cars, Mobile Phones and More'
-                                        className='find-search' />
-                                    Click here</button>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <div className="language">
-                                <h5>languages</h5>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <div className="login">
-                                <h5>Login</h5>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">OLX</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="placesearch">
+            <input type="text" />
+          </div>
+          <div className="search-product">
+            <input type="text" placeholder='Find Car and Mobiles' />
+            <button><i class="fa fa-search" aria-hidden="true"></i></button>
+          </div>
+          <div className="language">
+            <span>English</span>
+          </div>
+          <div className="login">
+            <span>Login</span>
+          </div>
+          <div className="sell">
+            <button className='sellproduct'>SELL</button>
+          </div>
         </div>
-    )
+      </nav>
+    </>
+  );
 }
 
-export default Header
+export default Header;
